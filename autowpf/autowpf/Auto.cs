4efe68bdc0f728661af8da028bbea56b5167968c
OneAndOnly #2 <72067053+GhostLead@users.noreply.github.com>
2025-04-04 08:54:10 +0200

    wpf projekt

---
+++ autowpf/autowpf/Auto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace autoapp
+{
+    internal class Auto
+    {
+        private int sorszam;
+        private string marka;
+        private string modell;
+        private int gyartasiEv;
+        private string szin;
+        private int eladottDarabSzam;
+        private int atlagosEladasiAr;
+
+        public int Sorszam { get => sorszam; set => sorszam = value; }
+        public string Marka { get => marka; set => marka = value; }
+        public string Modell { get => modell; set => modell = value; }
+        public int GyartasiEv { get => gyartasiEv; set => gyartasiEv = value; }
+        public string Szin { get => szin; set => szin = value; }
+        public int EladottDarabSzam { get => eladottDarabSzam; set => eladottDarabSzam = value; }
+        public int AtlagosEladasiAr { get => atlagosEladasiAr; set => atlagosEladasiAr = value; }
+
+        public Auto(string sor)
+        {
+            string[] tomb = sor.Split(';');
+
+            this.Sorszam = int.Parse(tomb[0]);
+            this.Marka = tomb[1];
+            this.Modell = tomb[2];
+            this.GyartasiEv = int.Parse(tomb[3]);
+            this.Szin = tomb[4];
+            this.EladottDarabSzam = int.Parse(tomb[5]);
+            this.AtlagosEladasiAr = int.Parse(tomb[6]);
+        }
+
+        public static List<Auto> Beolvas(string fajlNev)
+        {
+            List<Auto> autok = new List<Auto>();
+            string[] sorok = File.ReadAllLines(fajlNev);
+
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                autok.Add(new Auto(sorok[i]));
+            }
+
+            return autok;
+        }
+    }
+}
---
+++ autowpf/autowpf/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using autoapp;
+using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace autowpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //3-4. feladat: Betöltés gomb eseménykezelője
+        private void btnBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                List<Auto> autok = Auto.Beolvas(filePath);
+                dgTablazat.ItemsSource = autok;
+            }
+        }
+
+        //6. feladat: Keresés gomb eseménykezelője
+        private void btnBezar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Biztosan ki szeretne lépni?", "Kilépés", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                App.Current.Shutdown();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        //5. feladat: Szűrés eseménykezelője
+        private void txtEv_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lbSpecko.Items.Clear();
+
+            int szurtEv = int.Parse(txtEv.Text);
+
+            List<Auto> autok = (List<Auto>)dgTablazat.ItemsSource;
+
+            if (autok != null)
+            {
+                lbSpecko.Items.Clear();
+                var szurtAutok = autok.Where(a => a.GyartasiEv == szurtEv).ToList();
+                foreach (var auto in szurtAutok)
+                {
+                    lbSpecko.Items.Add($"{auto.Marka} {auto.Modell}");
+                }
+            }
+            
+
+
+        }
+    }
+}
